1a65f61b812953436ec7a86d1721dc4369d7db28
shiguren490 <60349708+shiguren490@users.noreply.github.com>
2020-02-23 22:58:05 +0900

    Update slick.js

--- slick.js
+++ slick.js
@@ -1,28 +1,32 @@
-jQuery(function($) {
-    $(document).ready(function(){
+jQuery(function ($) {
+    $(document).ready(function () {
         $('.slider').slick({
-            dots:true,
-            slidesToShow:4,
-            responsive:[
-                {
+            dots: true, // スライダー下部に表示される、ドット状のページネーションです
+            infinite: true, // 無限ループ
+            speed: 300, // 切り替わりのスピード
+            slidesToShow: 4, //通常 1024px以上の領域では4画像表示
+            slidesToScroll: 4,
+            responsive: [{
                     breakpoint: 1024,
-                    settings:{
-                        slidesToShow:3,
+                    settings: { //601px～1024pxでは3画像表示
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
                     }
-                },
+		},
                 {
-                    breakpoint: 768,
-                    settings:{
-                        slidesToShow:2,
+                    breakpoint: 600,
+                    settings: { //481px～600pxでは2画像表示
+                        slidesToShow: 2,
+                        slidesToScroll: 2
                     }
-                },
+		},
                 {
                     breakpoint: 480,
-                    settings:{
-                        slidesToShow:1,
+                    settings: { //480px以下では1画像表示
+                        slidesToShow: 1,
+                        slidesToScroll: 1
                     }
-                },
-            ]
+		}]
         });
     });
 });
