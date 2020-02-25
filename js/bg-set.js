ba5d8781e51e0d2c068d7ef58a27c5c1858babf3
shiguren490 <60349708+shiguren490@users.noreply.github.com>
2020-02-26 00:18:27 +0900

    Add files via upload

---
+++ js/bg-set.js
@@ -0,0 +1,5 @@
+jQuery(function($) {
+    $('header').bgSwitcher({
+      images: ["../img/hero.jpg", "../img/background2.jpg", "../img/background3.jpg", "../img/background4.jpg", "../img/background5.jpg"],
+    });
+});
