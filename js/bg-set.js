d11f2f3d9285a6a3fb7a81179860384f4761148b
shiguren490 <60349708+shiguren490@users.noreply.github.com>
2020-02-26 13:36:39 +0900

    Update bg-set.js

--- js/bg-set.js
+++ js/bg-set.js
@@ -1,5 +1,6 @@
-jQuery(function($) {
+jQuery(function ($) {
     $('header').bgSwitcher({
-      images: ["../img/hero.jpg", "../img/background2.jpg", "../img/background3.jpg", "../img/background4.jpg", "../img/background5.jpg"],
+        images: ["../img/hero.jpg", "../img/background2.jpg", "../img/background3.jpg", "../img/background4.jpg", "../img/background5.jpg"],
+        interval: 7000,
     });
 });
